ccd86df2ff44bee8d26928dd52b32012fe302654
Randy <happchu@126.com>
2021-04-22 07:01:37 +0800

    20210422再次修改networkmap刷新代码

--- trunk/user/rc/services.c
+++ trunk/user/rc/services.c
@@ -154,8 +154,9 @@ stop_networkmap(void)
 void
 restart_networkmap(void)
 {
-	if (pids("networkmap"))
-		doSystem("killall %s %s", "-KILL", "networkmap");
+	//if (pids("networkmap"))
+		//doSystem("killall %s %s", "-KILL", "networkmap");
+	stop_networkmap();
 	start_networkmap(0);
 }
 
